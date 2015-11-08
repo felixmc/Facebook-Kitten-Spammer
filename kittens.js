9bfe734af47701d35cdfd097f2cf2eac380838f9
Boris Aramis Aguilar Rodríguez <me@borisaguilar.com>
2015-11-08 15:59:44 -0600

    Selector and URL update
    Selector and URL update besides that, setTimeout didn't seem to correctly work with click() method on Firefox at least

--- kittens.js
+++ kittens.js
@@ -1,13 +1,20 @@
-var baseUrl  = 'http://www.randomkittengenerator.com/images/cats/rotator.php';
+var baseUrl  = 'http://www.randomkittengenerator.com/cats/rotator.php';
 var chatBox  = document.getElementsByName('message_body')[0];
-var replyBut = document.getElementById('u_0_s');
+var replyBut = document.getElementById('u_0_r');
 
 var kitInt   = setInterval(function() {
 	chatBox.classList.remove('DOMControl_placeholder');
 	chatBox.value = baseUrl + '?x=' + new Date().getTime();
-
+	
+	//this only seems to work on Chrome (Firefox doesn't seem to have this)
 	getEventListeners(chatBox).paste[0].listener();
-	setTimeout(replyBut.click, 4000);
 }, 6000);
 
-var stopKittens = function() { clearInterval(kitInt); };
+var replyClicker = setInterval(function(){
+	replyBut.click();
+}, 10000);
+
+var stopKittens = function() { 
+	clearInterval(kitInt);
+	clearInterval(replyClicker); 
+};
